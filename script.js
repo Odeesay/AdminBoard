baf0ff1b558cae90d26b9a716bcc6d1ac66d9f1a
Odeesay <oodeesay@gmail.com>
2022-05-18 18:58:51 +0300

    j

--- script.js
+++ script.js
@@ -237,8 +237,8 @@ const configBar = {
 // place.style.top = 0;
 //     place.style.fontSize = '10px';
 
-// $('#auth__login').click(function(){
-//       $('#auth__login__placeholder').css('bottom', '120px');
-//       $('#auth__login__placeholder').css('fontSize', '10px');
-// })
+$('#auth__login').click(function(){
+      $('#auth__login__placeholder').css('bottom', '120px');
+      $('#auth__login__placeholder').css('fontSize', '10px');
+})
 
